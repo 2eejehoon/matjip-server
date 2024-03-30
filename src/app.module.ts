5741a932caeabbf9a61499556e71b0a8a52ee51b
이제훈 <2eejehoon@gmail.com>
2024-03-30 17:47:00 +0900

    프로젝트 초기 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersController } from "./users/users.controller";
+import { UsersService } from "./users/users.service";
+import { UsersModule } from "./users/users.module";
+import { PostsService } from "./posts/posts.service";
+import { PostsModule } from "./posts/posts.module";
+import { PrismaService } from "./prisma/prisma.service";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UsersModule, PostsModule, PrismaModule],
+    controllers: [AppController, UsersController],
+    providers: [AppService, UsersService, PostsService, PrismaService]
 })
 export class AppModule {}
