9d9ce900f8da7e2ad97a076c4eb9d7cc510dcdbb
이제훈 <2eejehoon@gmail.com>
2024-04-02 16:30:55 +0900

    passport serialize, deserialize 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as session from "express-session";
+import * as passport from "passport";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api");
+    app.use(
+        session({
+            secret: process.env.PASSPORT_SECRET,
+            cookie: {
+                maxAge: 60000
+            }
+        })
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
     await app.listen(3000);
 }
 bootstrap();
