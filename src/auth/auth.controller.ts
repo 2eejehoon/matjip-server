a151be87a7d905fe5d8f68256b7a92d163b9f7f5
이제훈 <2eejehoon@gmail.com>
2024-04-02 22:34:50 +0900

    안쓰는 코드 삭제

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Req, UseGuards } from "@nestjs/common";
 import { GoogleAuthGuard } from "./google/GoogleAuthGuards";
 import { AuthService } from "./auth.service";
-import { Request } from "express";
 
 @Controller("auth")
 export class AuthController {
