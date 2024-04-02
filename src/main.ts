11ff2dad94c668ca0d77aea28a9c75c599ce980d
이제훈 <2eejehoon@gmail.com>
2024-04-02 22:35:25 +0900

    session 옵션, 로컬에서 개발할 때 cors 관련 설정

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,27 @@ import * as passport from "passport";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api");
+
+    // TODO: 로컬 환경에서 개발 끝나면 삭제
+    app.enableCors({
+        origin: ["http://localhost:3000"],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    });
+
     app.use(
         session({
             secret: process.env.PASSPORT_SECRET,
+            resave: false,
+            saveUninitialized: true,
             cookie: {
-                maxAge: 60000
+                sameSite: "lax",
+                maxAge: 60000,
+                httpOnly: true
             }
         })
     );
+
     app.use(passport.initialize());
     app.use(passport.session());
     passport.serializeUser((user, done) => {
@@ -22,6 +35,6 @@ async function bootstrap() {
     passport.deserializeUser((user, done) => {
         done(null, user);
     });
-    await app.listen(3000);
+    await app.listen(4000);
 }
 bootstrap();
