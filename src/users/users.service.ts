dafa9c9794bea690792cd791c477e172ae43feea
이제훈 <2eejehoon@gmail.com>
2024-04-03 12:27:51 +0900

    findUserEmail, createUser 출력 타입 명시

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from "@nestjs/common";
-import { Prisma } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class UsersService {
     constructor(private prisma: PrismaService) {}
 
-    async findUserByEmail(email: string) {
+    async findUserByEmail(email: string): Promise<User> {
         return this.prisma.user.findUnique({ where: { email } });
     }
 
-    async createUser(data: Prisma.UserCreateInput) {
+    async createUser(data: Prisma.UserCreateInput): Promise<User> {
         return this.prisma.user.create({ data });
     }
 }
