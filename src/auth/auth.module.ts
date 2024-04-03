9295a947f470543f4edc9ec89360592fde83491b
이제훈 <2eejehoon@gmail.com>
2024-04-03 23:52:03 +0900

    Jwt 토큰 발급

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,22 @@ import { AuthService } from "./auth.service";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { UsersModule } from "src/users/users.module";
 import { UsersService } from "src/users/users.service";
+import { JwtModule, JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
-    imports: [PrismaModule, UsersModule],
+    imports: [
+        PrismaModule,
+        UsersModule,
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                secret: config.get<string>("JWT_SECRET"),
+                signOptions: { expiresIn: 60000 }
+            })
+        })
+    ],
     controllers: [AuthController],
-    providers: [GoogleStrategy, AuthService, UsersService]
+    providers: [GoogleStrategy, AuthService, UsersService, JwtService]
 })
 export class AuthModule {}
--- src/auth/google/google-strategy.ts
+++ src/auth/google/google-strategy.ts
@@ -2,11 +2,10 @@ import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Profile, Strategy, VerifyCallback } from "passport-google-oauth20";
 import { AUTH_PROVIDERS_ENUM } from "src/contants/auth-providers";
-import { UsersService } from "src/users/users.service";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-    constructor(private usersService: UsersService) {
+    constructor() {
         super({
             clientID: process.env.AUTH_GOOGLE_CLIENT_ID,
             clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
@@ -25,17 +24,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
             authProviders: [AUTH_PROVIDERS_ENUM.GOOGLE]
         };
 
-        const exisitngUser = await this.usersService.findUserByEmail(user.email);
-        if (exisitngUser) {
-            if (!exisitngUser.authProviders.includes(AUTH_PROVIDERS_ENUM.GOOGLE)) {
-                const updatedUser = this.usersService.updateUserByEmail(user.email, exisitngUser);
-                done(null, updatedUser);
-            }
-
-            done(null, exisitngUser);
-        } else {
-            const createdUser = await this.usersService.createUser(user);
-            done(null, createdUser);
-        }
+        done(null, user);
     }
 }
