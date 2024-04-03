0248f1d24609f5b2b9bb3a915a663486a85cc894
이제훈 <2eejehoon@gmail.com>
2024-04-03 10:48:36 +0900

    validate 함수에서 user에 authProviders 추가

--- src/auth/google/GoogleStrategy.ts
+++ src/auth/google/GoogleStrategy.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Profile, Strategy, VerifyCallback } from "passport-google-oauth20";
-import { AuthService } from "../auth.service";
+import { AUTH_PROVIDERS_ENUM } from "src/contants/auth-providers";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-    constructor(private authService: AuthService) {
+    constructor() {
         super({
             clientID: process.env.AUTH_GOOGLE_CLIENT_ID,
             clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
@@ -14,22 +14,16 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
         });
     }
 
-    async validate(
-        accessToken: string,
-        refreshToken: string,
-        profile: Profile,
-        done: VerifyCallback
-    ) {
-        const { displayName, emails, photos } = profile;
-        try {
-            const user = await this.authService.googleLogin({
-                email: emails[0].value,
-                name: displayName,
-                photo: photos[0].value
-            });
-            done(null, user);
-        } catch (error) {
-            done(error, false);
-        }
+    async validate(accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) {
+        const { emails, displayName, photos } = profile;
+
+        const user = {
+            email: emails[0].value,
+            name: displayName,
+            photo: photos[0].value,
+            authProviders: [AUTH_PROVIDERS_ENUM.GOOGLE]
+        };
+
+        done(null, user);
     }
 }
