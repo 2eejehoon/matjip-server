be84a867bf7ad067a6b66fa5d8a270d4f8b13701
이제훈 <2eejehoon@gmail.com>
2024-04-04 17:07:56 +0900

    회원가입 기능

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get, Req, Res, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, Res, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
 import { GoogleAuthGuard } from "./google/google-auth-guard";
 import { AuthService } from "./auth.service";
 import { Request, Response } from "express";
 import { User } from "@prisma/client";
+import { SignupDto } from "./dto/signup.dto";
 
 @Controller("auth")
 export class AuthController {
     constructor(private authService: AuthService) {}
+    @Post("signup")
+    @UsePipes(ValidationPipe)
+    async signup(@Body() signupDto: SignupDto) {
+        return this.authService.signup(signupDto);
+    }
+
     @Get("google")
     @UseGuards(GoogleAuthGuard)
     async googleLogin(): Promise<void> {}
@@ -14,7 +21,7 @@ export class AuthController {
     @Get("google/callback")
     @UseGuards(GoogleAuthGuard)
     async googleLoginCallback(@Req() req: Request, @Res() res: Response): Promise<void> {
-        const user = await this.authService.findOrCreateUser(req.user as User);
+        const user = await this.authService.googleLogin(req.user as User);
 
         res.redirect(`${process.env.BASE_CLIENT_URL}/login/google-callback?accessToken=${user.accessToken}`);
     }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { User } from "@prisma/client";
 import { AUTH_PROVIDERS_ENUM } from "src/contants/auth-providers";
 import { UsersService } from "src/users/users.service";
+import { SignupDto } from "./dto/signup.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
@@ -11,7 +13,33 @@ export class AuthService {
         private jwtService: JwtService
     ) {}
 
-    async findOrCreateUser(user: User) {
+    async signup({ passwordConfirm, ...user }: SignupDto) {
+        if (user.password !== passwordConfirm) {
+            throw new HttpException("비밀번호와 비빌번호 확인이 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        const existingUser = await this.usersService.findUserByEmail(user.email);
+        if (existingUser) {
+            throw new HttpException("이미 존재하는 이메일 주소입니다.", HttpStatus.CONFLICT);
+        }
+
+        const data = {
+            ...user,
+            password: await bcrypt.hash(user.password, 10)
+        };
+
+        const createdUser = await this.usersService.createUser(data);
+
+        const { password, ...userinfo } = createdUser;
+        const payload = { email: userinfo.email, sub: userinfo.id };
+
+        return {
+            ...userinfo,
+            accessToken: this.jwtService.sign(payload, { secret: process.env.JWT_SECRET })
+        };
+    }
+
+    async googleLogin(user: User) {
         let existingUser = await this.usersService.findUserByEmail(user.email);
         if (existingUser) {
             if (!existingUser.authProviders.includes(AUTH_PROVIDERS_ENUM.GOOGLE)) {
