e84f508ba9170953be293fe81424ba21978b182b
이제훈 <2eejehoon@gmail.com>
2024-04-06 04:17:40 +0900

    JwtAuthGuard handleRequest 함수

--- src/auth/jwt/jwt-auth-guard.ts
+++ src/auth/jwt/jwt-auth-guard.ts
@@ -1,9 +1,17 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { User } from "@prisma/client";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
     canActivate(context: ExecutionContext) {
         return super.canActivate(context);
+    }
+
+    handleRequest(error: null | Error, user: User | false, info: Error): any {
+        if (error || !user) {
+            console.log(info);
+            throw error || new UnauthorizedException("유효하지 않은 토큰입니다.");
+        }
     }
 }
